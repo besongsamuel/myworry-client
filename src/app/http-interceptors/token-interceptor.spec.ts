d4ccf9ff2641bac8a9481a216511e431ea79eb39
Lord Aftermath <besongsamuel@yahoo.com>
2020-02-29 09:35:35 -0500

    Minor Changes

--- src/app/http-interceptors/token-interceptor.spec.ts
+++ src/app/http-interceptors/token-interceptor.spec.ts
@@ -1,7 +1,24 @@
 import { TokenInterceptor } from './token-interceptor';
+import { TestBed } from '@angular/core/testing';
+import { AuthService } from '../services/auth.service';
 
 describe('TokenInterceptor', () => {
+
+  let tokenIterceptor: TokenInterceptor;
+  let authServiceSpy: jasmine.SpyObj<AuthService>
+
+  beforeEach(() => {
+    const spy = jasmine.createSpy('AuthService');
+    TestBed.configureTestingModule({
+      providers: [{provide : AuthService, useValue: spy}]
+    });
+    authServiceSpy = TestBed.get(AuthService);
+    tokenIterceptor = new TokenInterceptor(authServiceSpy);
+    
+  });
+
   it('should create an instance', () => {
-    expect(new TokenInterceptor()).toBeTruthy();
+    expect(tokenIterceptor).toBeTruthy();
+    
   });
 });
