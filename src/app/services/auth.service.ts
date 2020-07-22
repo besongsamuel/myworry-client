fc014b2dc2ee962f5ce561c7f2e668c28fa678aa
Lord Aftermath <besongsamuel@yahoo.com>
2020-07-22 08:43:15 -0400

    .

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -158,7 +158,7 @@ export class AuthService {
   private handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
+      console.error('An error occurred:', error.error ? error.error.message : error.error);
     } else {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong,
